e89741674a33e1e6ea1ca423b215739024698ef1
nateliason <nateliason@gmail.com>
2021-03-29 13:50:17 -0500

    control flow, geolocation, file handling

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,5 @@
+function autocomplete(input, latInput, lngInput) {
+    // Wes explains how to do this in Video 16 but I skipped it because of the google maps billing
+}
+
+export default autocomplete;
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,24 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
+
+router.post('/add', 
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id', 
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
