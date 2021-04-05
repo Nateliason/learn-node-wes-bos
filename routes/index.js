d3321f31de9e0ebc5e47b9dd340d0e1c3edc0047
nateliason <nateliason@gmail.com>
2021-04-05 11:09:25 -0500

    added the Review model and set up posting to it from the store pages

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -45,8 +46,9 @@ router.post('/account/reset/:token',
     catchErrors(authController.update)
 );
 
-// Hearts
+// Hearts & Reviews
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 // API
 
